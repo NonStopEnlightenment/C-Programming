0fd2791002f354eaaf6c9d7d8dc25df2c3c3bb74
Non Stop Enlightenment <49872078+nse1982@users.noreply.github.com>
2020-03-30 16:00:24 +0530

    Update writing_char_to_file.c

--- writing_char_to_file.c
+++ writing_char_to_file.c
@@ -1,18 +1,18 @@
 #include<stdio.h>
 int main()
 {
-	FILE *fp;
+	FILE *fp;				//File Pointer
 	char c;
-	fp=fopen("sample.txt","w");
+	fp=fopen("sample.txt","w");		//Creating a new file
 	if ( fp == NULL)
-  {
-    perror("Error:");
-  }
-  else
+	{
+	    perror("Error:");
+	}
+  	else
 	{
 		while((c=getchar())!='*')
-			fputc(c,fp);
+			fputc(c,fp);		//Writing char by char to the file
 	}
-	fclose(fp);
+	fclose(fp);	//Closing the File
 	return (0);
 }
